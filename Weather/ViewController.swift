8fd7e3c100323d82eb3b22b04b1c205b7509b64e
AnLeoKiselev <anleokiselev@gmail.com>
2023-01-11 11:41:44 +0300

    1

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var rightAnchor1: NSLayoutConstraint?
-    var rightAnchor2: NSLayoutConstraint?
-    var leftAnchor1: NSLayoutConstraint?
-    var leftAnchor2: NSLayoutConstraint?
+    //var rightAnchor1: NSLayoutConstraint?
+   // var rightAnchor2: NSLayoutConstraint?
+    //var leftAnchor1: NSLayoutConstraint?
+    //var leftAnchor2: NSLayoutConstraint?
     var dayOrNight: String = ""
+    var firstConstraintsPosition: Bool = true
     
     private lazy var cityLabel: UILabel = {
         let label = UILabel()
@@ -50,8 +51,8 @@ class ViewController: UIViewController {
         //imageView.image = UIImage(named: "clouds")
         imageView.image = UIImage(named: "clear_sky_night")
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        //imageView.isUserInteractionEnabled = true
-        //imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleAnimate)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleAnimate)))
         return imageView
     } ()
     
@@ -97,12 +98,12 @@ class ViewController: UIViewController {
         view.addSubview(dayNightSegmentedControl)
     }
     
-    func handleAnimate() {
+    @objc func handleAnimate() {
         
-        rightAnchor1?.isActive.toggle()
-        leftAnchor1?.isActive.toggle()
-        rightAnchor2?.isActive.toggle()
-        leftAnchor2?.isActive.toggle()
+        //rightAnchor1?.isActive.toggle()
+        //leftAnchor1?.isActive.toggle()
+        //rightAnchor2?.isActive.toggle()
+        //leftAnchor2?.isActive.toggle()
 
         //rightAnchor2?.isActive = true
         //leftAnchor2?.isActive = true
@@ -110,22 +111,47 @@ class ViewController: UIViewController {
         //rightAnchor1?.isActive = false
         //leftAnchor1?.isActive = false
         
-            UIView.animate(withDuration: 1) {
-                self.view.layoutIfNeeded()
+      //  firstConstraintsPosition.toggle()
+//
+//        backgroundImageView.frame = CGRect(x: -400, y: 0, width: 1920, height: 1080)
+        
+        
+//
+           UIView.animate(withDuration: 1) {
+               self.backgroundImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -500).isActive = true
+               
+               self.view.layoutIfNeeded()
             }
-        }
- 
+        
+//        UIView.animate(withDuration: 10, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, animations: {
+//            self.view.layoutIfNeeded()
+//        })
+        
+        //backgroundImageView.centerYConstraint.constant = 500.0
+        
+        //self.view.layoutIfNeeded()
+
+//        UIView.animate(withDuration: Double(0.5), animations: {
+//           // self.backgroundImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 500).isActive = true
+//
+//
+//            self.backgroundImageView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//
+//                self.view.layoutIfNeeded()
+//            })
+//
+    }
     
     @objc func dayNightSegmentedControlDidChange(_ segmentedControl: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
             dayOrNight = "_day"
-            handleAnimate()
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         case 1:
             dayOrNight = "_night"
-            handleAnimate()
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         default:
@@ -137,38 +163,39 @@ class ViewController: UIViewController {
     @objc func weatherConditionsSegmentedControlDidChange(_ segmentedControl: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
-            backgroundImageView.image = UIImage(named: "clear_sky\(dayOrNight)")
-            handleAnimate()
+            backgroundImageView.image = UIImage(named: //"clear_sky\(dayOrNight)")
+            "c")
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         case 1:
             backgroundImageView.image = UIImage(named: "clouds\(dayOrNight)")
-            handleAnimate()
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         case 2:
             backgroundImageView.image = UIImage(named: "shower_rain\(dayOrNight)")
-            handleAnimate()
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         case 3:
             backgroundImageView.image = UIImage(named: "rain\(dayOrNight)")
-            handleAnimate()
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         case 4:
             backgroundImageView.image = UIImage(named: "thunderstorm\(dayOrNight)")
-            handleAnimate()
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         case 5:
             backgroundImageView.image = UIImage(named: "snow\(dayOrNight)")
-            handleAnimate()
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         case 6:
             backgroundImageView.image = UIImage(named: "mist\(dayOrNight)")
-            handleAnimate()
+            //handleAnimate()
             let generator = UISelectionFeedbackGenerator()
             generator.selectionChanged()
         default:
@@ -179,24 +206,44 @@ class ViewController: UIViewController {
     
     private func setSubviewsLayouts() {
         
-        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        backgroundImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        //backgroundImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        self.backgroundImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 000).isActive = true
         
-        leftAnchor1?.isActive = true
-        leftAnchor1 = backgroundImageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -400)//-400
         
-        rightAnchor1?.isActive = true
-        rightAnchor1 = backgroundImageView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 350) //350
+        //backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//
         
-        leftAnchor2?.isActive = false
-        leftAnchor2 = backgroundImageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -100) //-500
+        //backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+//
+        //backgroundImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        //backgroundImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        
+        //leftAnchor1?.isActive = true
+        //leftAnchor1
+        
+//        if firstConstraintsPosition {
+//
+//            backgroundImageView.frame = CGRect(x: -200, y: 0, width: 1920, height: 1080)
+////            backgroundImageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -400).isActive = true
+//
+//        } else {
+////
+//            backgroundImageView.frame = CGRect(x: -500, y: 0, width: 1920, height: 1080)
+//            backgroundImageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -400).isActive = true
+      //  }
+//
+       // rightAnchor1?.isActive = true
+        //rightAnchor1 = backgroundImageView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0) //350
+        
+//        leftAnchor2?.isActive = false
+//        leftAnchor2 = backgroundImageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0) //-500
 
-        rightAnchor2?.isActive = false
-        rightAnchor2 = backgroundImageView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 650) // 250
+        //rightAnchor2?.isActive = false
+        //rightAnchor2 = backgroundImageView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 650) // 250
         
-        
-        
-        
+//        backgroundImageView.frame = CGRect(x: -200, y: 0, width: 1920, height: 1080)
         
         cityLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
         cityLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
